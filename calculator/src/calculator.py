f60c7f2818aeeee3062f79a68071a9df98118a99
Erik Cespedes <cespedeserik87@gmail.com>
2025-09-04 20:13:39 -0300

    se agrega calculadora

---
+++ calculator/src/calculator.py
@@ -0,0 +1,14 @@
+from src import operations
+
+
+def calculate(operation, a, b):#realiza la operacion segun su operador
+    if operation == "+":
+        return operations.add(a,b)
+    elif operation == "-":
+        return operations.subtract(a,b)
+    elif operation == "*":
+        return operations.multiply(a,b)
+    elif operation == "/":
+        return operations.divide(a,b)
+    else:
+        return "Invalid operation"
