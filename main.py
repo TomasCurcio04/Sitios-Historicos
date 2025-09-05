7d3bcf91e0897d26bc4f069dcebceb8efe36fda8
Tomas Curcio <tomicurcio1@gmail.com>
2025-09-05 00:42:12 -0300

    Arreglo en el orden de las sentencias

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from calculator.src.calculator import calculate
 
 if __name__ == "__main__":
     num = float(input("Enter a number: "))
-    other_num = float(input("Enter another number: "))
     operation = input("Enter an operation (+, -, *, /): ")
+    other_num = float(input("Enter another number: "))
     result = calculate(operation, num, other_num)
     print(result)
