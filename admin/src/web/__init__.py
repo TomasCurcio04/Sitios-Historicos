11a3fc5c64e59eade853716132a5b45701d9413c
Erik Cespedes <cespedeserik87@gmail.com>
2025-10-01 06:10:50 -0300

    Agrego rutas

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,35 +1,56 @@
-from flask import Flask, abort ,render_template
+# src/web/__init__.py
+from flask import Flask, render_template, Blueprint
 import os
 from src.web.handlers import error
 from src.web.controllers.issues import bp as issues_bp
 from src.web.config import config
 
+# Creamos el blueprint principal
+web = Blueprint('web', __name__, template_folder='templates', static_folder='static')
 
-def create_app(env="development", static_folder=None):
-    if not static_folder:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        static_folder = os.path.abspath(os.path.join(base_dir, "..", "..", "static"))
+# Rutas del blueprint
+@web.route('/')
+def home():
+    return render_template("home.html")
+
+@web.route('/gestionsitioshistoricos')
+def gestionsitioshistoricos():
+    return render_template("gestionsitioshistoricos.html")
+
+@web.route('/validacion_propuesta')
+def validacion_propuesta():
+    return render_template("validacion_propuesta.html")
+
+@web.route('/moderacion_resenias')
+def moderacion_resenias():
+    return render_template("moderacion_resenias.html")
+
+@web.route('/gestion_usuarios')
+def gestion_usuarios():
+    return render_template("gestion_usuarios.html")
+
+
+def create_app(env="development"):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    static_folder = os.path.abspath(os.path.join(base_dir, "..", "..", "static"))
 
     app = Flask(
         __name__,
-        template_folder=os.path.join(os.path.dirname(__file__), "templates"),
+        template_folder=os.path.join(base_dir, "templates"),
         static_folder=static_folder
     )
+
+    # Configuración
     app.config.from_object(config[env])
     print(app.config)
 
-    @app.route('/')
-    def home():
-        return render_template("home.html")
-    
-     #Registrar blueprints
+    # Registrar blueprints
+    app.register_blueprint(web)
     app.register_blueprint(issues_bp)
-    
-    #Manejo de errores
+
+    # Manejo de errores
     app.register_error_handler(404, error.not_found)
     app.register_error_handler(401, error.not_authorized)
     app.register_error_handler(500, error.internal_server_error)
 
-   
-    
     return app
