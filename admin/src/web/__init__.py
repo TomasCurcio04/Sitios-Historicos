7b5b12e1142d4a3c2c1c99eaf1bd2aac76611af0
Erik Cespedes <cespedeserik87@gmail.com>
2025-10-02 20:30:02 -0300

    aplico formateo con Black

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -6,43 +6,48 @@ from src.web.controllers.issues import bp as issues_bp
 from src.web.config import config
 
 # Creamos el blueprint principal
-web = Blueprint('web', __name__, template_folder='templates', static_folder='static')
+web = Blueprint("web", __name__, template_folder="templates", static_folder="static")
+
 
 # Rutas del blueprint
-@web.route('/')
+@web.route("/")
 def home():
     return render_template("home.html")
 
-@web.route('/gestionsitioshistoricos')
+
+@web.route("/gestionsitioshistoricos")
 def gestionsitioshistoricos():
     return render_template("gestionsitioshistoricos.html")
 
-@web.route('/validacion_propuesta')
+
+@web.route("/validacion_propuesta")
 def validacion_propuesta():
     return render_template("validacion_propuesta.html")
 
-@web.route('/moderacion_resenias')
+
+@web.route("/moderacion_resenias")
 def moderacion_resenias():
     return render_template("moderacion_resenias.html")
 
-@web.route('/gestion_usuarios')
+
+@web.route("/gestion_usuarios")
 def gestion_usuarios():
     return render_template("gestion_usuarios.html")
 
 
 def create_app(env="development"):
     """Crea y devuelve la aplicación Flask según el entorno."""
-    
+
     # Determinar entorno por variable de entorno FLASK_ENV (por defecto 'development')
     env = os.environ.get("FLASK_ENV", "development")
-    
+
     base_dir = os.path.dirname(os.path.abspath(__file__))
     static_folder = os.path.abspath(os.path.join(base_dir, "..", "..", "static"))
 
     app = Flask(
         __name__,
         template_folder=os.path.join(base_dir, "templates"),
-        static_folder=static_folder
+        static_folder=static_folder,
     )
 
     # Configuración
