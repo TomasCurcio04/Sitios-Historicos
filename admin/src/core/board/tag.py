0aa29cd4b7435ca1d5d68a187eb3c8d61424a7da
Tomas Curcio <tomicurcio1@gmail.com>
2025-10-08 11:36:40 -0300

    Agrego relación de sitios con tags, genero slug y valido nombre del tag

--- admin/src/core/board/tag.py
+++ admin/src/core/board/tag.py
@@ -3,8 +3,10 @@
 from typing import TYPE_CHECKING
 from datetime import datetime, timezone
 from sqlalchemy import String, DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship, validates
 from src.core.database import Base
+from src.core.board.site import site_tag
+import unicodedata
 
 if TYPE_CHECKING:
     from src.core.board.site import Site
@@ -22,6 +24,36 @@ class Tag(Base):
         default=lambda: datetime.now(timezone.utc),
         nullable=False,
     )
+
+    sites: Mapped[list["Site"]] = relationship(
+    "Site",
+    secondary=site_tag,
+    back_populates="tag"
+)
+
+    def __init__(self, name: str):
+        self.name = name
+        self.slug = self.generate_slug(name)
+
+    @staticmethod
+    def generate_slug(name: str) -> str:
+        # Quita acentos
+
+        slug = unicodedata.normalize('NFKD', name).encode('ascii', 'ignore').decode('ascii')
+        # Convierte a minúsculas y reemplaza espacios por guiones
+        slug = slug.lower().replace(" ", "-")
+        return slug
+    
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value or not value.strip():
+            raise ValueError("El nombre es obligatorio")
+        value = value.strip()
+        if len(value) < 3 or len(value) > 50:
+            raise ValueError("El nombre debe tener entre 3 y 50 caracteres")
+        return value
+
+    
 
     def __repr__(self):
         return f"<Tag(id_tag={self.id_tag}, name='{self.name}')>"
