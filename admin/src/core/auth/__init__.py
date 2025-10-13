541c02dab34ec20814322f5b65dbb412b0a0efa0
Javier Costas <fercostas81@gmail.com>
2025-10-13 17:47:11 -0300

    Corrección mvc de feature flags

--- admin/src/core/auth/__init__.py
+++ admin/src/core/auth/__init__.py
@@ -222,4 +222,25 @@ def modify_feature_flag(name, enabled, updated_by, maintenance_message=None):
     return flag
 
 
+def update_feature_flags(flags_data, updated_by):
+    """Función para actualizar múltiples feature flags."""
+    flags = list_feature_flags()
+    has_changes = False
+    for flag in flags:
+        flag_id = str(flag.id)
+        if flag_id in flags_data:
+            new_enabled = flags_data[flag_id].get('enabled', False)
+            new_message = flags_data[flag_id].get('maintenance_message', '')
+            
+            if flag.enabled != new_enabled or flag.maintenance_message != new_message:
+                flag.enabled = new_enabled
+                flag.maintenance_message = new_message
+                flag.updated_by = updated_by
+                has_changes = True
+    
+    if has_changes:
+        db.session.commit()
+    return flags
+
+
 ####Fin de funciones de feature flags###
--- admin/src/web/controllers/feature_flags.py
+++ admin/src/web/controllers/feature_flags.py
@@ -2,8 +2,6 @@
 """Módulo controlador de feature flags"""
 
 from flask import request, render_template, redirect, url_for, flash, Blueprint, session
-import logging
-from src.core.database import db
 from src.web.utils import admin_maintenance_required
 from src.core import auth
 
@@ -15,20 +13,17 @@ feature_flags_bp = Blueprint("feature_flags", __name__, url_prefix="/featureflag
 def feature_flags():
     """Vista del menu de feature flags."""
     flags = auth.list_feature_flags()
-    try:
-        usuario = auth.buscar_usuario(session.get("user"))
-        print(f"usuario: {usuario}")
-    except Exception as e:
-        print(f"Error al buscar usuario: {e}")
-
+    usuario = auth.buscar_usuario(session.get("user"))
     usuario_id = usuario.id_user if usuario else 1
     print(f"usuario_id: {usuario_id}")
     if request.method == "POST":
+        flags_data = {}
         for flag in flags:
-            flag.enabled = f"enabled_{flag.id}" in request.form
-            flag.maintenance_message = request.form.get(f"mensaje_{flag.id}", "")
-            flag.updated_by = usuario_id
-        db.session.commit()
+            flags_data[str(flag.id)] = {
+                "enabled": f"enabled_{flag.id}" in request.form,
+                "maintenance_message": request.form.get(f"mensaje_{flag.id}", ""),
+            }
+        auth.update_feature_flags(flags_data, usuario_id)
         flash("Feature flags actualizados correctamente", "success")
         return redirect(url_for("feature_flags.feature_flags"))
 
