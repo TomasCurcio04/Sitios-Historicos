ff0c89e845f3bedf0f487170d15b20fbc96946e4
Javier Costas <fercostas81@gmail.com>
2025-10-14 17:40:34 -0300

    Modificacion de la db para usar neon

--- admin/src/web/config.py
+++ admin/src/web/config.py
@@ -1,6 +1,6 @@
 """Configuration classes for different environments."""
 
-from os import environ
+# from os import environ
 
 
 class BaseConfig:
@@ -24,18 +24,26 @@ class ProductionConfig(BaseConfig):
 class DevelopmentConfig(BaseConfig):
     """Development configuration class."""
 
+    # SECRET_KEY = "your_dev_secret_key"
+    # DB_USER = "postgres"
+    # DB_PASSWORD = "KcooNtcHPuxNsQSXpQfMuUiVpmEFaeYm"
+    # DB_NAME = "railway"
+    # DB_HOST = "nozomi.proxy.rlwy.net"
+    # DB_PORT = "55215"
+    # DB_SCHEME = "postgresql+psycopg2"
+    # DEBUG = True
     SECRET_KEY = "your_dev_secret_key"
-    DB_USER = "postgres"
-    DB_PASSWORD = "KcooNtcHPuxNsQSXpQfMuUiVpmEFaeYm"
-    DB_NAME = "railway"
-    DB_HOST = "nozomi.proxy.rlwy.net"
-    DB_PORT = "55215"
+    DB_USER = "neondb_owner"
+    DB_PASSWORD = "npg_RAUO1X2TMZad"
+    DB_NAME = "neondb"
+    DB_HOST = "ep-red-river-a828fhqc-pooler.eastus2.azure.neon.tech"
+    DB_PORT = "5432"
     DB_SCHEME = "postgresql+psycopg2"
     DEBUG = True
+    DB_SSL_PARAMS = "sslmode=require&channel_binding=require"
     SQLALCHEMY_ENGINES = {
-        #  "default": "postgresql+psycopg2://grupo10:GtGouFR0ONveaoqQKi31@172.19.0.3:5432/grupo10?sslmode=require&channel_binding"
-        # "default": f"{DB_SCHEME}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-        "default": "postgresql+psycopg2://neondb_owner:npg_RAUO1X2TMZad@ep-red-river-a828fhqc-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
+        "default": f"{DB_SCHEME}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}{f'?{DB_SSL_PARAMS}' if DB_SSL_PARAMS else ''}"
+        # "default": "postgresql+psycopg2://neondb_owner:npg_RAUO1X2TMZad@ep-red-river-a828fhqc-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require"
     }
 
 
