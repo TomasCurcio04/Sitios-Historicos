0cc8c92b2729e670c51da1be6163dcdaf65d9cc7
Javier Costas <fercostas81@gmail.com>
2025-10-14 21:44:50 -0300

    Correccion de feature flags de usuario

--- admin/src/web/utils.py
+++ admin/src/web/utils.py
@@ -1,7 +1,7 @@
 # pylint: disable=import-error
 """Utilidades para la aplicación web."""
 
-from flask import flash, session as current_user, abort
+from flask import flash, session as current_user, abort, redirect, url_for
 from src.core import auth
 
 
@@ -12,11 +12,15 @@ def admin_maintenance_required(view):
     def wrapped_view(*args, **kwargs):
         flag = auth.get_feature_flag("admin_maintenance_mode")
         usuario = auth.buscar_usuario(current_user.get("user"))
-        print(usuario.s_user)
-        if not usuario or not (usuario.s_user or usuario.role == 1):
-            abort(401)
+        # SI no es usuario, no puede entrar.
+        if not usuario:
+            return redirect(url_for("auth.login"))
+
+        # Si el flag está activado, solo s_user puede entrar
         if flag and flag.enabled:
-            flash(flag.maintenance_message or "Mantenimiento", "warning")
+            if not usuario.s_user:
+                abort(403)
+        flash(flag.maintenance_message or "Mantenimiento", "warning")
         return view(*args, **kwargs)
 
     return wrapped_view
