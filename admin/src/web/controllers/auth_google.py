240cf3d5fa02380a39eb406ba29e2181ff815d97
franciscoGarrubba <franciscogarrubba@gmail.com>
2025-12-02 17:01:06 -0300

    Modifico para dejar de utilizar session por JWT

--- admin/src/web/controllers/auth_google.py
+++ admin/src/web/controllers/auth_google.py
@@ -1,19 +1,22 @@
 from flask import Blueprint, redirect, url_for, session, render_template, request
 from src.core.services.auth.user_serv import buscar_usuario_public, crear_user_public
 from src.web.oauth import oauth
+from flask import current_app
+import jwt
 
 
 bp = Blueprint("google_auth", __name__, url_prefix="/google")
-# Prueba despues de agregar google_id
+
 @bp.route("/login")
 def login():
-    session['next'] = request.args.get('next') or request.referrer
-    redirect_uri = url_for("google_auth.auth", _external=True)
+    next_url = request.args.get('next') or request.referrer or '/'
 
+    redirect_uri = url_for("google_auth.auth", _external=True)
     print("Redirect URI:", url_for("google_auth.auth", _external=True))
     return oauth.google.authorize_redirect(
         redirect_uri,
-        prompt="select_account"
+        prompt="select_account",
+        state=next_url
     )
 
 @bp.route("/login/callback")
@@ -31,23 +34,19 @@ def auth():
             picture = userinfo.get("picture")
         )
     
-    session['user'] = {
-        "id": userinfo.get("sub"),
+    playload = {
+        "public_user_id": userinfo.get("sub"),
         "email": userinfo.get("email"),
         "name": userinfo.get("name"),
-        "picture": userinfo.get("picture"),
-        "type": "google"
+        "picture": userinfo.get("picture")
     }
-    next_url = session.get('next') or '/'
-    return redirect(next_url)
+    jwt_token = jwt.encode(playload, current_app.config["JWT_SECRET_KEY"], algorithm="HS256")
+
+    next_url = request.args.get('state') or '/'
+    separator = '&' if '?' in next_url else '?'
+    return redirect(f"{next_url}{separator}auth_token={jwt_token}")
     
 @bp.route("/logout")
 def logout():
-    session.pop("user", None)
     next_url = request.args.get("next", "/")
     return redirect(next_url)
-
-@bp.route("/status")
-def status():
-    user = session.get("user")
-    return {"logged_in": bool(user), "user": user}
